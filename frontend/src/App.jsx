991b88555696e5823d55ca7bff881b7feca2b41d
John <dejhswavyguy@gmail.com>
2025-08-01 21:52:59 -0500

    remove boilerplate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-
   return (
     <>
       <h1 className='text-blue-500'>New text</h1>
