47ddf433966c615f285a17997d775b01b96c99a8
John <dejhswavyguy@gmail.com>
2025-08-07 15:35:10 -0500

    add nsbe name and yellow bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,11 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='text-blue-500'>New text</h1>
+      <div className='w-full h-10 bg-yellow-300 fixed top-0 left-0'></div>
+      <div className='fixed top-20 left-10 border border-1 border-yellow-500 w-30 h-25 sm:left-1 bg-black opacity-75 text-white flex justify-center box-border py-3 flex-col font-[Space_Grotesk]'>
+        <span className='text-3xl text-center'>NSBE</span>
+        <span className='text-center text-md font-thin'>Grambling Chapter</span>
+      </div>
     </>
   )
 }
