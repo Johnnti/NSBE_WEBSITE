c49d9bc9b7aa29e76c921c1dd05c4f8189883afb
John <dejhswavyguy@gmail.com>
2025-08-19 23:47:25 -0500

    finish formatting of Page5 text

--- frontend/src/pages/Page5.jsx
+++ frontend/src/pages/Page5.jsx
@@ -1,12 +1,12 @@
 export default function Page5(){
     return (
         <div className="relative text-white w-[100vw] min-w-screen h-full bg-yellow-500 min-h-screen bg-fixed flex flex-col gap-y-10 p-20">
-            <span>Our Programs</span>
-            <div>
-                <span>We Offer Variosu Programs to Support Student Success</span>
-                <a href="#">Explore Programs</a>
+            <span className="text-black text-2xl">Our Programs</span>
+            <div className="flex flex-row justify-between">
+                <p className="text-5xl w-150 text-black">We Offer Various Programs to Support Student Success</p>
+                <a href="#" className="bg-yellow-300 h-fit p-5 text-black font-bold hover:text-yellow-500 hover:bg-black">Explore Programs</a>
             </div>
-            <p>NSBE Grambling is dedicated to offering a range of programs and initiatives tailored to the needs of Black engineering students. From academic support to professional development, we are committed to equipping our members with the tools for success.</p>
+            <p className="w-100">NSBE Grambling is dedicated to offering a range of programs and initiatives tailored to the needs of Black engineering students. From academic support to professional development, we are committed to equipping our members with the tools for success.</p>
         
         </div>
 
